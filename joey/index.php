e3d89ce093dd36540446e1a3788326d71ea3c47e
Rob Korobkin <rob.korobkin@gmail.com>
2017-08-07 23:54:42 -0400

    Centering main content in Joey's theme.

--- joey/index.php
+++ joey/index.php
@@ -30,7 +30,7 @@
 
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/truncated.css" />
-		
+
 
     <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 
@@ -73,7 +73,7 @@
 				</nav>
 			</header>
 
-      <section class="container" style="margin-top: 20px">
+      <section class="container" style="margin-top: 20px; text-align: center; padding-top: 50px">
         <section class="box">
 					<?php
 						if ( have_posts() ) {
